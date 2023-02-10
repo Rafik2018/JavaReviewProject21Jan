1f3fad65574f8890287fda77f1d1ebacf75b1eca
Rafik2018 <ismayilovrafik1@gmail.com>
2023-02-09 19:12:25 -0500

    Class6

---
+++ src/Review_6/StringBuilderexample.java
@@ -0,0 +1,25 @@
+package Review_6;
+
+public class StringBuilderexample {
+
+
+    public static void main(String[] args) {
+
+
+
+            //this class is mutable collection of characters
+            StringBuilder sb=new StringBuilder("Hello");
+
+            //all changes happen on the same object
+            sb.reverse(); //proves that string builder is mutable
+
+            System.out.println(sb); // olleH
+
+            String str=sb.toString();
+            str.toUpperCase(); //proves that String is immutable
+            System.out.println(str);//olleH
+
+    }
+
+
+}
